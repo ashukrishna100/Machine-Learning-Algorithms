978e0d9b38367b149ccab70baf3bd67b77f5870f
ashukrishna100 <43339535+ashukrishna100@users.noreply.github.com>
2019-03-27 17:41:43 +0530

    Add files via upload

--- simple linear regression.py
+++ simple linear regression.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
+# In[31]:
 
 
 import pandas as pd
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[129]:
+# In[32]:
 
 
 df = pd.read_csv("forestfires.csv")
@@ -17,7 +17,7 @@ df = df[['temp','area']]
 df.head()
 
 
-# In[82]:
+# In[33]:
 
 
 ## y = b_0 + b_1x
@@ -25,7 +25,7 @@ x = df.temp
 y = df.area
 
 
-# In[136]:
+# In[34]:
 
 
 ##visualize the data
@@ -34,7 +34,7 @@ plt.ylim(-1, 10)
 plt.show()
 
 
-# In[137]:
+# In[35]:
 
 
 def estimate_coef(x,y):
@@ -59,7 +59,7 @@ def estimate_coef(x,y):
 print(estimate_coef(x,y))
 
 
-# In[125]:
+# In[36]:
 
 
 def correlation(x,y):
@@ -81,27 +81,27 @@ def least_squares_fit(x, y):
 least_squares_fit(x,y)
 
 
-# In[69]:
+# In[37]:
 
 
 b = estimate_coef(x,y)
 b
 
 
-# In[25]:
+# In[38]:
 
 
 y_pred=df['temp'].apply(lambda a:b[0] + b[1]*a)
 y_pred
 
 
-# In[43]:
+# In[39]:
 
 
 plt.scatter(x,y_pred,color= 'm')
 
 
-# In[78]:
+# In[40]:
 
 
 plt.xlabel('temperature')
@@ -110,34 +110,126 @@ plt.plot(x,y_pred,color= 'm')
 plt.show()
 
 
-# In[79]:
+# In[41]:
 
 
 ## Using sklearn
 from sklearn import linear_model
 
 
-# In[111]:
+# In[42]:
 
 
 lm = linear_model.LinearRegression()
 model = lm.fit(np.array(x).reshape(-1,1),np.array(y).reshape(-1,1))
 
 
-# In[140]:
+# In[43]:
 
 
 y_predicted = lm.predict(np.array(x).reshape(-1,1))
 y_predicted
 
 
-# In[144]:
+# In[44]:
 
 
 plt.xlabel('temperature')
 plt.ylabel('area')
 plt.plot(x,y_predicted,color='m')
 plt.show()
+
+
+# In[45]:
+
+
+#coefficient/slope
+print(lm.coef_)
+
+
+# In[46]:
+
+
+#intercept
+print(lm.intercept_)
+
+
+# ## Performance of the model
+
+# In[47]:
+
+
+## the higher the R-squared, the better the model fits your data
+## r2 square score
+from sklearn.metrics import mean_squared_error, r2_score
+import math
+
+
+# In[48]:
+
+
+##lesser the RMSE better the model
+mse = mean_squared_error(y , y_predicted)
+print("mean squared error is :",mse)
+print("root mean squared error is :",math.sqrt(mse))
+
+
+# In[49]:
+
+
+## higher the r2 score better the model
+r2_score(y , y_predicted)
+
+
+# ## GRADIENT DESCENT
+
+# In[50]:
+
+
+#### GRADIENT DESCENT ########
+
+m = 0
+c = 0
+
+L = 0.001  # The learning Rate
+epochs = 100  # The number of iterations to perform gradient descent
+
+n = float(len(x)) # Number of elements in X
+
+# Performing Gradient Descent 
+for i in range(epochs): 
+    Y_pred = m*x + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(x * (y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c
+    
+print (m, c)
+
+
+# In[51]:
+
+
+y_pred=df['temp'].apply(lambda a:c + m*a)
+y_pred.head()
+
+
+# In[52]:
+
+
+plt.plot(x,y_pred,color='m')
+
+
+# In[53]:
+
+
+mean_squared_error(y , y_pred)
+
+
+# In[ ]:
+
+
+
 
 
 # In[ ]:
